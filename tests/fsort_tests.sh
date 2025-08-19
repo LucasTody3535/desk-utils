bd283dff240a24c5faa2cccce0d405ddc627edd5
Toodie <lucas.vtodt@gmail.com>
2025-08-19 03:39:20 -0300

    test: streamline ms_office files tests

--- tests/fsort_tests.sh
+++ tests/fsort_tests.sh
@@ -2,76 +2,41 @@
 
 source ./utils.sh
 
-test_word_files_sorting() {
-    local base_dir="./target/ms_office/word"
-    printf "\n  > Word files sorting\n"
+test_ms_office_files_sorting() {
+    local base_dir="./target/ms_office"
+    printf "\n  > MS Office files sorting\n"
 
-    printf "    > Documents sorting..."
-    it_exists_in_path "$base_dir"/documents/file.doc "doc"
-    it_exists_in_path "$base_dir"/documents/file.docx "docx"
+    printf "    > Word files sorting..."
+    it_exists_in_path "$base_dir"/word/documents/file.doc "doc"
+    it_exists_in_path "$base_dir"/word/documents/file.docx "docx"
+    it_exists_in_path "$base_dir"/word/templates/file.dot "dot"
+    it_exists_in_path "$base_dir"/word/templates/file.dotx "dotx"
+    it_exists_in_path "$base_dir"/word/macros/file.docm "docm"
+    it_exists_in_path "$base_dir"/word/macros/file.dotm "dotm"
     printf "OK\n"
 
-    printf "    > Templates sorting..."
-    it_exists_in_path "$base_dir"/templates/file.dot "dot"
-    it_exists_in_path "$base_dir"/templates/file.dotx "dotx"
+    printf "    > Excel files sorting..."
+    it_exists_in_path "$base_dir"/excel/documents/file.xls "xls"
+    it_exists_in_path "$base_dir"/excel/documents/file.xlsx "xlsx"
+    it_exists_in_path "$base_dir"/excel/templates/file.xlt "xlt"
+    it_exists_in_path "$base_dir"/excel/templates/file.xltx "xltx"
+    it_exists_in_path "$base_dir"/excel/macros/file.xlm "xlm"
+    it_exists_in_path "$base_dir"/excel/macros/file.xlsm "xlsm"
+    it_exists_in_path "$base_dir"/excel/macros/file.xltm "xltm"
     printf "OK\n"
 
-    printf "    > Macros sorting..."
-    it_exists_in_path "$base_dir"/macros/file.docm "docm"
-    it_exists_in_path "$base_dir"/macros/file.dotm "dotm"
-    printf "OK\n"
-
-    printf "\n"
-}
-
-test_excel_files_sorting() {
-    local base_dir="./target/ms_office/excel"
-    printf "  > Excel files sorting\n"
-
-    printf "    > Documents sorting..."
-    it_exists_in_path "$base_dir"/documents/file.xls "xls"
-    it_exists_in_path "$base_dir"/documents/file.xlsx "xlsx"
-    printf "OK\n"
-
-    printf "    > Templates sorting..."
-    it_exists_in_path "$base_dir"/templates/file.xlt "xlt"
-    it_exists_in_path "$base_dir"/templates/file.xltx "xltx"
-    printf "OK\n"
-
-    printf "    > Macros sorting..."
-    it_exists_in_path "$base_dir"/macros/file.xlm "xlm"
-    it_exists_in_path "$base_dir"/macros/file.xlsm "xlsm"
-    it_exists_in_path "$base_dir"/macros/file.xltm "xltm"
-    printf "OK\n"
-
-    printf "\n"
-}
-
-test_powerpoint_files_sorting() {
-    local base_dir="./target/ms_office/power_point"
-    printf "  > Power Point files sorting\n"
-
-    printf "    > Documents sorting..."
-    it_exists_in_path "$base_dir"/documents/file.ppt "ppt"
-    it_exists_in_path "$base_dir"/documents/file.pptx "pptx"
-    printf "OK\n"
-
-    printf "    > Templates sorting..."
-    it_exists_in_path "$base_dir"/templates/file.pot "pot"
-    it_exists_in_path "$base_dir"/templates/file.potx "potx"
-    printf "OK\n"
-
-    printf "    > Macros sorting..."
-    it_exists_in_path "$base_dir"/macros/file.pptm "pptm"
-    it_exists_in_path "$base_dir"/macros/file.potm "potm"
-    it_exists_in_path "$base_dir"/macros/file.ppsm "ppsm"
-    it_exists_in_path "$base_dir"/macros/file.sldm "sldm"
-    printf "OK\n"
-
-    printf "    > Slideshows sorting..."
-    it_exists_in_path "$base_dir"/slideshows/file.pps "pps"
-    it_exists_in_path "$base_dir"/slideshows/file.ppsx "ppsx"
-    it_exists_in_path "$base_dir"/slideshows/file.sldx "sldx"
+    printf "    > Power Point files sorting..."
+    it_exists_in_path "$base_dir"/power_point/documents/file.ppt "ppt"
+    it_exists_in_path "$base_dir"/power_point/documents/file.pptx "pptx"
+    it_exists_in_path "$base_dir"/power_point/templates/file.pot "pot"
+    it_exists_in_path "$base_dir"/power_point/templates/file.potx "potx"
+    it_exists_in_path "$base_dir"/power_point/macros/file.pptm "pptm"
+    it_exists_in_path "$base_dir"/power_point/macros/file.potm "potm"
+    it_exists_in_path "$base_dir"/power_point/macros/file.ppsm "ppsm"
+    it_exists_in_path "$base_dir"/power_point/macros/file.sldm "sldm"
+    it_exists_in_path "$base_dir"/power_point/slideshows/file.pps "pps"
+    it_exists_in_path "$base_dir"/power_point/slideshows/file.ppsx "ppsx"
+    it_exists_in_path "$base_dir"/power_point/slideshows/file.sldx "sldx"
     printf "OK\n"
 
     printf "\n"
@@ -186,10 +151,7 @@ setup
 cd ./target || exit
 fsort &> /dev/null && cd ..
 
-test_word_files_sorting
-test_excel_files_sorting
-test_powerpoint_files_sorting
-
+test_ms_office_files_sorting
 test_acrobat_files_sorting
 
 test_writer_files_sorting
